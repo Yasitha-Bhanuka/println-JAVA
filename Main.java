2ba4b95a521caaeea9b7c03b3d6053c6ff6726ff
yasitha <it22563750@my.sliit.lk>
2023-07-06 13:54:22 +0530

    Add more code
    Can see the avoid new line using print method.

--- Main.java
+++ Main.java
@@ -3,7 +3,18 @@ public class Main {
 
         // using these println method automatically next statement go to the new line.
         System.out.println("robin");
-        System.out.println("age 22");
-        System.out.println("University student");
+        System.out.println("-age 22");
+        System.out.println("-University student");
+        System.out.println(-2001);
+        System.out.println("-Graduate in 2025");
+
+        // using print method for it.
+        System.out.print("robin");
+        System.out.print("-age 22");
+        System.out.print("-University student");
+        System.out.print(-2001);
+        System.out.print("-Graduate in 2025");
+
+        // it is display avoiding goto new lines next statements.
     }
 }
