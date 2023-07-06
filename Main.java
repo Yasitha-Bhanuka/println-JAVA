b3fcf39fcfacc2b2efd39445d293faa34e9d90d4
yasitha <it22563750@my.sliit.lk>
2023-07-06 13:39:18 +0530

    Create Main.java
    using println();

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args){
+
+        // using these println method automatically next statement go to the new line.
+        System.out.println("robin");
+        System.out.println("age 22");
+        System.out.println("University student");
+    }
+}
