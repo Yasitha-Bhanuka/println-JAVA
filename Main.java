3408cc6d2c6c0cfdbe2cbb979e496fe4f9dedd88
yasitha <it22563750@my.sliit.lk>
2023-07-06 13:46:50 +0530

    Update Main.java
    check the differances using print() method

--- Main.java
+++ Main.java
@@ -3,7 +3,12 @@ public class Main {
 
         // using these println method automatically next statement go to the new line.
         System.out.println("robin");
-        System.out.println("age 22");
-        System.out.println("University student");
+        System.out.println("-age 22");
+        System.out.println("-University student");
+
+        // using print method it is not automatically go through the next line.
+        System.out.print("robin");
+        System.out.print("-age 22");
+        System.out.println("-University student");
     }
 }
